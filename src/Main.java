5ee8edfb9bf46b6bd011b82b63be3fbe28e8c581
HanaYabuki <1847196775@qq.com>
2022-04-25 15:18:41 +0800

    manual input

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,26 @@
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        for (String s : args) {
+        if (args.length == 0) {
+            Scanner in = new Scanner(System.in);
+            StringBuilder sb = new StringBuilder();
+            while (true) {
+                String next = in.nextLine();
+                if (next.equals("")) {
+                    List<LexToken> lexTokens = Lexer.ins.analyze(sb.toString());
+                    Lexer.ins.sout(lexTokens);
+                    FileUtil.ins.saveLex("tmp.analyze.txt", lexTokens);
+                    return;
+                }
+                else {
+                    sb.append(next);
+                    sb.append('\n');
+                }
+            }
+        }
+        else for (String s : args) {
             System.out.println(s);
             String data = FileUtil.ins.read(s);
             List<LexToken> lexTokens = Lexer.ins.analyze(data);
